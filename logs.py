70f9b951f261ab51db56c1e640f2908b2b6231df
AHMAD <ahmad.mahmod.5492216@gmail.com>
2020-05-12 20:17:31 +0300

    ADD .1 fies

--- logs.py
+++ logs.py
@@ -13,10 +13,10 @@ import os                   # Load the Library Module
 from time import strftime   # Load just the strftime Module from Time
 
 logsdir="/var/log/syslog"      # Set the Variable logsdir
-zip_program="zip.exe"       # Set the Variable zip_program - 1.1
+zip_program="zip.exe"          # Set the Variable zip_program - 1.1
 
 for files in os.listdir(logsdir):                       # Find all the files in the directory
-	if files.endswith(".log"):                          # Check to ensure the files in the directory end in .log
+	if files.endswith(".log" or ".1"):                          # Check to ensure the files in the directory end in .log
 		files1=files+"."+strftime("%Y-%m-%d")+".zip"    # Create the Variable files1, this is the files in the directory, then we add a suffix with the date and the zip extension
 		os.chdir(logsdir)                               # Change directory to the logsdir
 		os.system(zip_program + " " +  files1 +" "+ files)  # Zip the logs into dated zip files for each server. - 1.1
